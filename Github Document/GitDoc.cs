572e815b95581aecaba2c99b4efba9de07716ca0
Bruger <Bruger@DESKTOP-HTDA39G>
2024-08-30 09:25:44 +0200

    Add project files.

---
+++ Github Document/GitDoc.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Github_Document
+{
+    internal class GitDoc
+    {
+        public void print()
+        {Console.WriteLine("Malthe")}
+    }
+}
